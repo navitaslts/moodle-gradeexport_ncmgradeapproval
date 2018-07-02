6e353f91695ba73f427c5683ed2c2d22a5350e92
Nicolas JOURDAIN <nicolasjourdain@Nicolass-MacBook-Pro.local>
2018-07-02 12:03:34 +1000

    Initial commit

---
+++ lang/en/gradeexport_ncmgradeapproval.php
@@ -0,0 +1,32 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'ncmgradeapproval', language 'en', branch 'MOODLE_20_STABLE'
+ *
+ * @package   ncmgradeapproval
+ * @copyright 2018 onwards Nicolas Jourdain  {@link http://navitas.com}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['eventgradeexported'] = 'NCM Grade Approval exported';
+$string['pluginname'] = 'NCM Grade Approval Export';
+$string['timeexported'] = 'Last downloaded from this course';
+$string['ncmgradeapproval:publish'] = 'Publish NCM grade approval export';
+$string['ncmgradeapproval:view'] = 'Use NCM grade approval export';
+$string['gradebooknoexport'] = 'The gradebook is not locked. You cannot export the gradebook';
+$string['gradebookexport'] = 'The gradebook is locked. You can export the gradebook';
