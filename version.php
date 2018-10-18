0e9373bd102df8c0f26434dae671f90a00b0f3bd
Nicosoft <nicolas.jourdain@gmail.com>
2018-10-18 15:01:46 +1100

    Bump version 1.0.0

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2018092800;        // The current plugin version (Date: YYYYMMDDXX).
-$plugin->release = 'v0.0.1';
+$plugin->version   = 2018101800;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->release = 'v1.0.0';
 $plugin->requires  = 2017111300;        // Requires this Moodle version.
 $plugin->maturity = MATURITY_STABLE;
 $plugin->component = 'gradeexport_ncmgradeapproval'; // Full name of the plugin (used for diagnostics).
